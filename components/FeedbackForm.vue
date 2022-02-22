432534866b4d47ad52715ac265e65d625591f26e
Tara Z. Manicsic <tzmanics@gmail.com>
2022-02-22 00:43:00 -0500

    moves to component

---
+++ components/FeedbackForm.vue
@@ -0,0 +1,20 @@
+        <template>
+        <form
+          name="contact"
+          method="post"
+          action="/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+  <input type="hidden" name="form-name" value="contact" />
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,5 @@
 <template>
-        <form
-          name="contact"
-          method="post"
-          action="/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-  <input type="hidden" name="form-name" value="contact" />
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+<FeedbackForm />
 </template>
 
 <script>
