dcf22dc618094a843ddfcb1039e4d3690bae0038
Tara Z. Manicsic <tzmanics@gmail.com>
2022-02-21 23:54:51 -0500

    does the things

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
